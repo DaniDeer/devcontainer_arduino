20ee7f50d1150572cdbcdb3218c2b5dbe0dd73f0
Daniel Hirsch <danirocks@hotmail.de>
2024-08-07 11:07:23 +0000

    Change baud rate in hello-world example

--- src/hello-world/hello-world.ino
+++ src/hello-world/hello-world.ino
@@ -12,4 +12,12 @@ void setup() {
 
 void loop() {
   // main code
+
+   // This should match the serial speed configured in VSCode
+  //Serial.begin(9600);
+
+  // Allow time for the serial monitor to start
+  delay(3000);
+
+  Serial.println("Hello World!");
 }
