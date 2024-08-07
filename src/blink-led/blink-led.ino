2be3c6958f33fa1fffd53739ca5e08f02de36cbf
Daniel Hirsch <danirocks@hotmail.de>
2024-08-07 13:31:12 +0000

    Union led and hello-world into one sketch for testing uploading and serial monitor

--- src/blink-led/blink-led.ino
+++ src/blink-led/blink-led.ino
@@ -1,12 +1,27 @@
 #define LED_BUILTIN 2  // Onboard LED pin on NodeMCU
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED pin as an output
+  // put your setup code here, to run once:
+
+  pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED pin as an 
+
+  // This should match the serial speed configured in VSCode
+  Serial.begin(115200);
+
+  // Allow time for the serial monitor to start
+  delay(3000);
+
+  Serial.println("Hello World!");
+
 }
 
 void loop() {
+  // main code
+
   digitalWrite(LED_BUILTIN, LOW);  // Turn the LED on
   delay(1000);                     // Wait for a second
   digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off
+  Serial.println("Blink!");
   delay(1000);                     // Wait for a second
+
 }
